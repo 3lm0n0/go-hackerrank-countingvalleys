b87acb900c5fefa6f52b50f9f64998bf2f781e59
3lm0n0 <pedro.jose.bidoggia@hotmail.com>
2023-03-23 23:50:52 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(countingValleys(int32(6), string("UDDDUDUU")))
+}
+
+func countingValleys(steps int32, path string) int32 {
+	chars := strings.Split(path, "")
+	var condition bool
+	down := string("D")
+	sum := int32(0)
+	valleysCount := int32(0)
+	for i := int32(0); i < steps; i++ {
+		condition = chars[i] == down
+		sum += int32(1)
+		if condition {
+			sum -= int32(2)
+		}
+		if i > int32(0) && sum == int32(0) && !condition {
+			valleysCount += int32(1)
+		}
+	}
+	return valleysCount
+}
